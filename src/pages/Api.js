3dcf64a834d21d147e7d5a115b71b0ed45567008
Romina Rosales <rromina758@yahoo.com>
2023-11-17 10:08:40 -0300

    clasefin

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -9,14 +9,14 @@ const Api =props =>{
     useEffect(()=>{
         const cargarDatos =async()=>{
             setCargando(true);
-            const res=(await fetch('https://rickandmortyapi.com/api/character')).json();
+            const res= await (await fetch('https://rickandmortyapi.com/api/character')).json();
             setPersonajes(res.results);
             setCargando(false);
         }
         cargarDatos();
     },[]);
     
-
+// fetch > para traer los datos nativos de JS. Tambien se puede utilizar axios (instalar dependencia)
 
 
     return(
@@ -24,26 +24,28 @@ const Api =props =>{
         <h2>Datos Api</h2>
         {cargando ? <p>cargando datos...</p> :( 
         <div className="personajes">
+        {personajes.map(personaje=>(
            
-            <div className="personaje">
-                <h4>Nombre del personaje</h4>
+            <div className="personaje" key={personaje.id}>
+                <h4>{personaje.name}</h4>
                 <div className="ficha">
                     <div className="foto">
-                        <img src= "https://fakeimg.pl/100x100" alt=""/>
+                        <img src= {personaje.image} alt={personaje.name}/>
 
                     </div>
                     <div className="datos">
-                        <h6>Especie: Humano</h6>
-                        <h6>Vivo: Si o No</h6>
+                        <h6>especie: {personaje.species === 'Human'? 'Humano' : personaje.species}</h6>
+                        <h6>Vivo: {personaje.status === 'Alive'? 'si': 'no' }</h6>
 
                     </div>
 
                 </div>
 
             </div>
-           
+           ))}
 
         </div>
+        
         )}
 
         </>
